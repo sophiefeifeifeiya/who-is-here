bdaee841f6ee2af217aa22114b790b0d15746dff
rexjz <robbort.xu@gmail.com>
2022-04-22 14:08:59 +0800

    add comments

--- lib/utils/Touchable.dart
+++ lib/utils/Touchable.dart
@@ -3,15 +3,11 @@
 import 'package:flutter/widgets.dart';
 
 Widget makeTouchable(Widget w, VoidCallback calllBack) {
-  // return GestureDetector(
-  //   onTap: calllBack,
-  //   child: w,
-  // );
-
   return GestureDetector(
     behavior: HitTestBehavior.translucent,
     onTap: calllBack,
     child: AbsorbPointer(
+      // if it is the child of GestureDetector which is one of children of a Stack，it shold be wrapped by AbsorbPointer
       child: w,
     ),
   );
